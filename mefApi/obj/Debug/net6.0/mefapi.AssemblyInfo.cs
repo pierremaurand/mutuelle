39e2b64b154437b07156264486cd172ddfc64fc3
Pierre Maurand Ovassa <pierremaurand@gmail.com>
2025-03-13 12:55:42 +0100

    new file:   dotnet-install.sh 	new file:   mefApi/bin/Debug/net6.0/mefapi 	modified:   mefApi/bin/Debug/net6.0/mefapi.deps.json 	modified:   mefApi/bin/Debug/net6.0/mefapi.dll 	modified:   mefApi/bin/Debug/net6.0/mefapi.pdb 	modified:   mefApi/bin/Debug/net6.0/mefapi.runtimeconfig.json 	modified:   mefApi/bin/Debug/net6.0/mefapi.staticwebassets.runtime.json 	modified:   mefApi/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   mefApi/obj/Debug/net6.0/apphost 	modified:   mefApi/obj/Debug/net6.0/mefapi.AssemblyInfo.cs 	modified:   mefApi/obj/Debug/net6.0/mefapi.AssemblyInfoInputs.cache 	modified:   mefApi/obj/Debug/net6.0/mefapi.GeneratedMSBuildEditorConfig.editorconfig 	modified:   mefApi/obj/Debug/net6.0/mefapi.GlobalUsings.g.cs 	modified:   mefApi/obj/Debug/net6.0/mefapi.MvcApplicationPartsAssemblyInfo.cs 	modified:   mefApi/obj/Debug/net6.0/mefapi.assets.cache 	modified:   mefApi/obj/Debug/net6.0/mefapi.csproj.AssemblyReference.cache 	modified:   mefApi/obj/Debug/net6.0/mefapi.csproj.CoreCompileInputs.cache 	modified:   mefApi/obj/Debug/net6.0/mefapi.csproj.FileListAbsolute.txt 	modified:   mefApi/obj/Debug/net6.0/mefapi.dll 	modified:   mefApi/obj/Debug/net6.0/mefapi.genruntimeconfig.cache 	modified:   mefApi/obj/Debug/net6.0/mefapi.pdb 	new file:   mefApi/obj/Debug/net6.0/mefapi.sourcelink.json 	modified:   mefApi/obj/Debug/net6.0/ref/mefapi.dll 	modified:   mefApi/obj/Debug/net6.0/refint/mefapi.dll 	modified:   mefApi/obj/Debug/net6.0/staticwebassets.build.json 	modified:   mefApi/obj/Debug/net6.0/staticwebassets.development.json 	new file:   mefApi/obj/Debug/net6.0/staticwebassets.pack.json 	new file:   mefApi/obj/Debug/net6.0/staticwebassets/msbuild.build.mefapi.props 	new file:   mefApi/obj/Debug/net6.0/staticwebassets/msbuild.buildMultiTargeting.mefapi.props 	new file:   mefApi/obj/Debug/net6.0/staticwebassets/msbuild.buildTransitive.mefapi.props 	new file:   mefApi/obj/Debug/net6.0/staticwebassets/msbuild.mefapi.Microsoft.AspNetCore.StaticWebAssets.props 	modified:   mefApi/obj/mefapi.csproj.nuget.dgspec.json 	modified:   mefApi/obj/mefapi.csproj.nuget.g.props 	modified:   mefApi/obj/mefapi.csproj.nuget.g.targets 	modified:   mefApi/obj/project.assets.json 	modified:   mefApi/obj/project.nuget.cache

--- mefApi/obj/Debug/net6.0/mefapi.AssemblyInfo.cs
+++ mefApi/obj/Debug/net6.0/mefapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mefapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99cee8a6c6cb389776c643d46ed0a78041698463")]
 [assembly: System.Reflection.AssemblyProductAttribute("mefapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mefapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
